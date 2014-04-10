79e1229b3d88ed5edf20334a26e73dbed4e23c1d
Jon Chen <bsd@voltaire.sh>
2014-04-09 21:52:02 -0400

    fix typo

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    email = db.Columnt(db.VARCHAR(320))
+    email = db.Column(db.VARCHAR(320))
 
     def __init__(self,
                  name=name,
