537d2469c70a347318e43dcdc641231d9ebbe7c4
Jon Chen <bsd@voltaire.sh>
2014-04-10 11:14:46 -0400

    init restful

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask.ext import restful
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+api = restful.Api(app)
 db = SQLAlchemy(app)
 
 from app import models, views, forms
