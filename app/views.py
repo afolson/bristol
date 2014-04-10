8d9a9eb516cd291c5a38bb836387886ba6da1b3b
Jon Chen <bsd@voltaire.sh>
2014-04-10 12:10:31 -0400

    clean up views and add new post

--- app/views.py
+++ app/views.py
@@ -1,32 +1,18 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from flask.ext import uploads
 from app import app, db, login_manager
 from app.models import User
-from app.forms import SignupForm
+from app.forms import SignupForm, PoopForm
 
-
-@app.before_request():
-    g.user = current_user
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/signup')
-def signup():
-    form = SignupForm(request.form)
+@app.route('/post')
+def post():
+    form = PoopForm(request.form)
     if request.method == 'POST' and form.validate():
-        user = User(form.username.data,
-                    form.name.data,
-                    form.password.data,
-                    form.email.data)
-        db.session.add(user)
-        flash('Thanks, have fun shittin!')
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('signup.html',
-                           form=form)
+        uploads.save(request.files['upload'])
+        return redirect(url_for('index'))
+    return render_template('post.html', form=form)
