2aeabeac864f0c29a13d79a0a43b1a6f1da0c0fa
Jon Chen <bsd@voltaire.sh>
2014-04-09 22:37:01 -0400

    fix importing

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app, db
-from forms import SignupForm
-from models import User
+from app.models import User
+from app.forms import SignupForm
 
 
 @app.route('/')
