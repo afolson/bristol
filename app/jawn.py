02adc6820b65c6ec8413243bf963fe3769915037
Jon Chen <bsd@voltaire.sh>
2014-04-12 11:36:18 -0400

    make sure models is defined

--- app/jawn.py
+++ app/jawn.py
@@ -1,5 +1,5 @@
 from app import models, manager
 
-manager.create_api(User, methods=['GET', 'POST', 'DELETE'])
-manager.create_api(Poop, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(models.User, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(models.Poop, methods=['GET', 'POST', 'DELETE'])
 
